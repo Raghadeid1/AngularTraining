17187d8ea2224aeb8110673a5cda948e749f1ca7
unknown <raghadkka@gmail.com>
2023-04-17 11:52:20 +0200

    Animations Update

---
+++ app.module.ts
@@ -0,0 +1,69 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { CourseComponent } from './course/course.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { SummaryPipe } from './pipes/summary.pipe';
+import { InputComponent } from './input/input.component';
+import { PanelComponent } from './panel/panel.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { SwitchComponent } from './switch/switch.component';
+import { Favorite2Component } from './favorite2/favorite2.component';
+import { StyleComComponent } from './style-com/style-com.component';
+import { InputFormatDirective } from './input-format.directive';
+import { FormComponent } from './form/form.component';
+import { CommonModule } from '@angular/common';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttprequestsComponent } from './httprequests/httprequests.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { AnimationsComponent } from './animations/animations.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    CourseComponent,
+    FavoriteComponent,
+    SummaryPipe,
+    InputComponent,
+    PanelComponent,
+    DirectivesComponent,
+    SwitchComponent,
+    Favorite2Component,
+    StyleComComponent,
+    InputFormatDirective,
+    FormComponent,
+    RxjsComponent,
+    HttprequestsComponent,
+    NavBarComponent,
+    OrdersComponent,
+    ProductsComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    ShoppingCartComponent,
+    AnimationsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
